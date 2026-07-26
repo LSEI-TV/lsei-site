98af294a52f5817c1c2830af6fd521ca72808c18
LSEI TV <marc.poolos@gmail.com>
2026-07-26 02:13:01 +0200

    Auto : publication programmee (articles a date future caches) + IndexNow (indexation Bing instantanee)

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -5,7 +5,7 @@ import { site } from '../data/site';
 // Flux RSS des actualités : permet de s'abonner aux articles et aux agrégateurs
 // de les reprendre. Accessible sur /rss.xml
 export async function GET(context) {
-  const articles = (await getCollection('articles', ({ data }) => !data.draft))
+  const articles = (await getCollection('articles', ({ data }) => !data.draft && data.date.valueOf() <= Date.now()))
     .sort((a, b) => b.data.date.valueOf() - a.data.date.valueOf());
 
   return rss({
